263afc8e677c8b2a3b68b615dd57d71c8db262b5
cgalo <cgalo13@gmail.com>
2020-09-24 17:36:35 -0400

    Created SearchBar component and added it on the App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import SearchBar from './SearchBar/SearchBar';
 
 function App() {
   return (
-    <div>App!</div>
+    <div>
+      <SearchBar/>
+    </div>
   );
 }
 
