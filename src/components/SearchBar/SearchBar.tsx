50aecf40f4bca84475ca4da10dc94bf432b14d92
cgalo <cgalo13@gmail.com>
2020-09-25 14:54:18 -0400

    Updated index.html, App & Searchbar componenets
    Overview:
    - Added SemanticUI css link to the index.html
    - For the app component added a className to adjust the size of the SearchBar component in it
    -  For the search bar compoenent I created the render function and added the className to apply SemanticUI's styling for search bars

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-
-function SearchBar (){
-    return (
-        <div>
-            SearchBar
-        </div>
-    );
+class SearchBar extends React.Component{
+     render (){
+         return (
+             <div className="ui segment">
+                 <form className="ui form">
+                     <div className="field">
+                         <label> Image Search</label>
+                        <input type="text"/>
+                     </div>
+                     
+                </form>
+             </div>
+         );
+     }
 }
 
 export default SearchBar;
